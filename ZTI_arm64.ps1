eeaba33100fce743c1b83eebd4e1f96fe725acf8
Mirko Schnellbach <mirko.schnellbach@4iot.de>
2025-11-17 15:10:32 +0100

    Switched ARM config to Kiosk mode

--- ZTI_arm64.ps1
+++ ZTI_arm64.ps1
@@ -3,7 +3,7 @@ $OSName = 'Windows 11 25H2 x64'
 $OSEdition = 'Pro'
 $OSActivation = 'Retail'
 $OSLanguage = 'de-de'
-$GroupTag = 'AutopilotUserDeployment'
+$GroupTag = 'AutopilotKioskDeployment'
 $TimeZone = 'W. Europe Standard Time'
 $TimeServerUrl = "https://www.timeapi.io/api/time/current/zone?timeZone=Europe%2FBerlin"
 $OutputFile = "X:\AutopilotHash.csv"
